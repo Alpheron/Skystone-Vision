f3666911f4493da98175ea4c7b8158651cee5d9d
Aditya Mangalampalli <aditya.mangalampalli@gmail.com>
2019-11-19 22:37:52 -0800

    Tools for building

--- PB_file.py
+++
@@ -1,3 +0,0 @@
-from common import *
-
-freeze_graph('tf_example', '/workdir/src/Skystone-Vision/SkystoneVision/model.config', '/workdir/src/Skystone-Vision/SkystoneVision/model_weights/model.ckpt', '/workdir/src/Skystone-Vision/Output', input_shape=None) 
