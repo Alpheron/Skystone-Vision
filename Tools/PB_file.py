6b9e2bde556b6aa42f570654ac2ed9fd3a34ce94
Aditya Mangalampalli <aditya.mangalampalli@gmail.com>
2019-12-18 07:19:49 +0000

    MY LIFE

--- Tools/PB_file.py
+++ Tools/PB_file.py
@@ -1,3 +1,3 @@
 from common import *
 #change the tf example shit to image-tensor
-freeze_graph('image_tensor', '/workdir/src/Skystone-Vision/Skystone-Rev5/Universal/model.config', '/workdir/src/Skystone-Vision/Skystone-Rev5/Universal/model_weights/model.ckpt', '/workdir/src/Skystone-Vision/Skystone-Rev5/image_tensor/PB_file', input_shape=None) 
+freeze_graph('image_tensor', '/workdir/src/Skystone-Vision/Skystone-Rev6/Universal/model.config', '/workdir/src/Skystone-Vision/Skystone-Rev6/Universal/model_weights/model.ckpt', '/workdir/src/Skystone-Vision/Skystone-Rev6/image_tensor/PB_file', input_shape=None) 
