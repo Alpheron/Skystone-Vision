d92d91182bca2e127726165b6ee74e90d18d4275
Aditya Mangalampalli <aditya.mangalampalli@gmail.com>
2019-12-18 08:23:32 -0800

    Eat Bread; Need video

---
+++ Final/Python/new.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+cvNet = cv.dnn.readNetFromTensorflow('/Users/Tinku/Desktop/RoboticsShit/Skystone-Vision/Final/image_tensor/PB_file/model.pb', '/Users/Tinku/Desktop/RoboticsShit/Skystone-Vision/Final/image_tensor/Pbtxt/model.pbtxt')
+
+img = cv.imread('/Users/Tinku/Desktop/pls.jpeg')
+rows = img.shape[0]
+cols = img.shape[1]
+cvNet.setInput(cv.dnn.blobFromImage(img, size=(300, 300), swapRB=True, crop=False))
+cvOut = cvNet.forward()
+
+for detection in cvOut[0,0,:,:]:
+    score = float(detection[2])
+    if score > 0.99:
+        left = detection[3] * cols
+        top = detection[4] * rows
+        right = detection[5] * cols
+        bottom = detection[6] * rows
+        cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=5)
+
+cv.imshow('img', img)
+cv.waitKey()
