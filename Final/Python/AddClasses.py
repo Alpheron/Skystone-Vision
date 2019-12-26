fa4db2b5408ec42e445786798766dbc4fd824aba
Aditya Mangalampalli <aditya.mangalampalli@gmail.com>
2019-12-26 01:12:31 -0800

    Basic Framework

--- Final/Python/AddClasses.py
+++ Final/Python/AddClasses.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 from collections import defaultdict
 cvNet = cv.dnn.readNetFromTensorflow('/Users/Tinku/Desktop/RoboticsShit/Skystone-Vision/Final/image_tensor/PB_file/model.pb', '/Users/Tinku/Desktop/RoboticsShit/Skystone-Vision/Final/image_tensor/Pbtxt/model.pbtxt')
-img = cv.imread('/Users/Tinku/Desktop/TestStuff/work.jpeg')
+img = cv.imread('/Users/Tinku/Desktop/TestStuff/pls.jpeg')
 
 rows = img.shape[0]
 cols = img.shape[1]
@@ -12,28 +12,30 @@ objects_dict = {}
 for detection in cvOut[0,0,:,:]:
     score = float(detection[2])
     if score > 0.75:
-        class_id = detection[1]
-        if (class_id==3):
-            Stonetype = "Skystone"
-        elif (class_id==4):
-            Stonetype = "Stone"
-        print(Stonetype)
+        objectClass = detection[1]
         left = detection[3] * cols
         top = detection[4] * rows
         right = detection[5] * cols
         bottom = detection[6] * rows
-        objects_dict[score]=[left,top,right,bottom]
+        objects_dict[score]=[left,top,right,bottom, objectClass]
         numOfRect += 1
         
 l=list(objects_dict.keys())
 l.sort(reverse=True)
 for i in l[:6] :
     #print(i,objects_dict[i])
+    if objects_dict[i][4] == 3.0:
+        detectedObject = "Skystone"
+    elif objects_dict[i][4] == 4.0:
+        detectedObject = "Stone"
+    elif objects_dict[i][4] == 2.0:
+        detectedObject = "Red Foundation"
+    elif objects_dict[i][4] == 1.0:
+        detectedObject = "Blue Foundation"
     cv.rectangle(img, (int(objects_dict[i][0]), int(objects_dict[i][1])), (int(objects_dict[i][2]), int(objects_dict[i][3])), (23, 230, 210), thickness=5)
-    #print(Stonetype)
-    #cv.putText(img, Stonetype, (int(objects_dict[i][0]), int(objects_dict[i][1])), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2, cv.LINE_AA)
+    #print(detectedObject)
+    cv.putText(img, detectedObject, (int(objects_dict[i][0]), int(objects_dict[i][1])), cv.FONT_HERSHEY_SIMPLEX, 5, (0,0,0), 10, cv.LINE_AA)
 
-
-#cv.imshow('img', img)
+cv.imshow('img', img)
 #print(numOfRect)
-#cv.waitKey()
+cv.waitKey()
